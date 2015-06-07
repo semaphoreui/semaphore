409b52de143c688d89baa7fa977acaf3b68361a1
Raphael MESSNER <raphael.messner@gmail.com>
2015-06-07 08:04:33 +0200

    Write a key pair by identity name

--- lib/scripts/pullGit.sh
+++ lib/scripts/pullGit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-printf "#\041/bin/bash\nssh -i /root/.ssh/id_rsa \$1 \$2\n" > /root/ssh_wrapper.sh
-chmod +x /root/ssh_wrapper.sh
+printf "#\041/bin/bash\nssh -i /root/.ssh/$3 \$1 \$2\n" > /root/ssh_wrapper_$3.sh
+chmod +x /root/ssh_wrapper_$3.sh
 
 cd /root
-GIT_SSH=/root/ssh_wrapper.sh git clone "$1" $2
+GIT_SSH=/root/ssh_wrapper_$3.sh git clone "$1" $2
 if [ $? -ne 0 ]; then
 	exit 2
 fi
