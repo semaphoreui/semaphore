67a29f763bfc0d3ef6989cb09a4dac0c0faf4f83
Matej Kramny <matejkramny@gmail.com>
2016-04-08 20:41:20 +0100

    Running all the things 🎉
    - Define extra (environment) vars to tasks
    - Able to create custom ansible jobs by defining parameters (infinite usabilities for user here)
    - Log output to db
    - Debug tasks
    - Ssh key for accessing inventory (separate from the key to access inventory api)
    -

--- models/Environment.go
+++ models/Environment.go
@@ -3,10 +3,10 @@ package models
 import "github.com/ansible-semaphore/semaphore/database"
 
 type Environment struct {
-	ID        int    `db:"id" json:"id"`
-	ProjectID int    `db:"project_id" json:"project_id"`
-	Password  string `db:"password" json:"password"`
-	JSON      string `db:"json" json:"json"`
+	ID        int     `db:"id" json:"id"`
+	ProjectID int     `db:"project_id" json:"project_id"`
+	Password  *string `db:"password" json:"password"`
+	JSON      string  `db:"json" json:"json"`
 }
 
 func init() {
