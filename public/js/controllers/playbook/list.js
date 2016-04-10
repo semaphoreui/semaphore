062086456f54068dc665b58054e93f5f1ba5474d
Matej Kramny <matejkramny@gmail.com>
2016-04-10 19:45:27 +0100

    Rimraf legacy stuff

--- public/js/controllers/playbook/list.js
+++
@@ -1,8 +0,0 @@
-define([
-	'app',
-	'services/playbooks'
-], function(app) {
-	app.registerController('PlaybooksCtrl', ['$scope', 'playbooks', function($scope, playbooks) {
-		$scope.playbooks = playbooks.playbooks;
-	}]);
-});
