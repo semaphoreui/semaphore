8326e3668a9677f9d18fa010dd6a8b3241284a35
William GARCIA <garcia.rodriguez.william@gmail.com>
2016-08-01 17:10:00 +1000

    Fix DB migration - alter table

--- db/migrations/v1.1.0.sql
+++ db/migrations/v1.1.0.sql
@@ -1,2 +1,2 @@
 /* update precision of output logs - also prevents confusion when sorting the table to guarantee it is in the same order */
-alter table task__output change `time` `time` datetime(6) not null;
+alter table task__output change `time` `time` datetime not null;
