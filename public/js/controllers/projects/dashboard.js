6731829792da8094ceaadef6e0c34ddd495a915b
Matej Kramny <matejkramny@gmail.com>
2016-12-21 19:12:35 +0900

    parse time with momentjs - fix #197

--- public/js/controllers/projects/dashboard.js
+++ public/js/controllers/projects/dashboard.js
@@ -1,38 +1,51 @@
-define(['controllers/projects/taskRunner'], function () {
-	app.registerController('ProjectDashboardCtrl', ['$scope', '$http', 'Project', '$uibModal', '$rootScope', function ($scope, $http, Project, $modal, $rootScope) {
-		$http.get(Project.getURL() + '/events').success(function (events) {
-			$scope.events = events;
-		});
+define(['controllers/projects/taskRunner'], function() {
+    app.registerController('ProjectDashboardCtrl', ['$scope', '$http', 'Project', '$uibModal', '$rootScope', function($scope, $http, Project, $modal, $rootScope) {
+        $http.get(Project.getURL() + '/events').success(function(events) {
+            $scope.events = events;
 
-		$scope.reload = function () {
-			$http.get(Project.getURL() + '/tasks').success(function (tasks) {
-				$scope.tasks = tasks;
+            events.forEach(function(evt) {
+                evt.createdFormatted = moment(evt.created).format('DD/M/YY HH:MM')
+            })
+        });
 
-				$scope.tasks.forEach(function (t) {
-					if (!t.start || !t.end) {
-						return;
-					}
+        $scope.reload = function() {
+            $http.get(Project.getURL() + '/tasks').success(function(tasks) {
+                $scope.tasks = tasks;
 
-					// t.duration = moment(t.start).from(moment(t.end), true);
-					t.duration = moment(t.start).diff(moment(t.end), 'minutes');
-				});
-			});
-		}
-		$scope.reload();
+                $scope.tasks.forEach(function(t) {
+                    if (t.created) {
+                        t.createdFormatted = moment(t.created).format('DD/M/YY HH:MM')
+                    }
+                    if (t.start) {
+                        t.startFormatted = moment(t.start).format('DD/M/YY HH:MM:ss')
+                    }
+                    if (t.end) {
+                        t.endFormatted = moment(t.end).format('DD/M/YY HH:MM:ss')
+                    }
 
-		$scope.openTask = function (task) {
-			var scope = $rootScope.$new();
-			scope.task = task;
-			scope.project = Project;
+                    if (!t.start || !t.end) {
+                        return;
+                    }
 
-			$modal.open({
-				templateUrl: '/tpl/projects/taskModal.html',
-				controller: 'TaskCtrl',
-				scope: scope,
-				size: 'lg'
-			}).result.then(function () {
-				$scope.reload();
-			});
-		}
-	}]);
+                    t.duration = moment(t.start).diff(moment(t.end), 'minutes');
+                });
+            });
+        }
+        $scope.reload();
+
+        $scope.openTask = function(task) {
+            var scope = $rootScope.$new();
+            scope.task = task;
+            scope.project = Project;
+
+            $modal.open({
+                templateUrl: '/tpl/projects/taskModal.html',
+                controller: 'TaskCtrl',
+                scope: scope,
+                size: 'lg'
+            }).result.then(function() {
+                $scope.reload();
+            });
+        }
+    }]);
 });
