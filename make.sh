e14da14b3b5888980eb074c83a2edcb66a9d3b2f
Matej Kramny <matejkramny@gmail.com>
2017-04-18 16:51:02 +0100

    add octocat body
    pug bug not there anymore

--- make.sh
+++ make.sh
@@ -29,7 +29,7 @@ EOF
 
 	cd public
 	lessc css/semaphore.less > css/semaphore.css
-	pug html/*.pug html/*/*.pug html/*/*/*.pug
+	pug $(find ./html/ -name "*.pug")
 	cd -
 fi
 
@@ -72,7 +72,7 @@ if [ "$1" == "watch" ]; then
 
 	nodemon -w js -i bundle.js -e js bundler.js &
 	nodemon -w css -e less --exec "lessc css/semaphore.less > css/semaphore.css" &
-	pug -w -P html/*.pug html/*/*.pug html/*/*/*.pug &
+	pug -w -P $(find ./html/ -name "*.pug") &
 
 	cd -
 	reflex -r '\.go$' -R '^public/vendor/' -R '^node_modules/' -s -d none -- sh -c 'go build -i -o /tmp/semaphore_bin cli/main.go && /tmp/semaphore_bin'
