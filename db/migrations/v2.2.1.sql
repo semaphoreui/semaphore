7c04a926d52624834c9694c7b2336a28b1110d70
Matej Kramny <matejkramny@gmail.com>
2017-04-18 16:42:00 +0100

    fix 2.2.1 migration #299

--- db/migrations/v2.2.1.sql
+++ db/migrations/v2.2.1.sql
@@ -1,5 +1,3 @@
-ALTER TABLE `project__user` ADD PRIMARY KEY(project_id);
-
 ALTER TABLE `project__template_schedule` ADD PRIMARY KEY(template_id);
 
 ALTER TABLE `task__output` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
