a881404687214b009566b8b20c03728eaba05f77
Tom Whiston <tom.whiston@gmail.com>
2018-03-08 00:42:11 +0100

    add version code generator remove version.go from vcs add task for generating version

---
+++ util/version_gen/generator.go
@@ -0,0 +1,43 @@
+// +build ignore
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var versionTmpl = `package util
+
+//The Semaphore build version
+var Version = "{{ .VERSION }}"
+`
+
+func main(){
+
+	if len(os.Args) <= 1 {
+		log.Fatalln("Must pass in version number")
+	}
+
+	data := make(map[string]string)
+	data["VERSION"] = os.Args[1]
+
+	tmpl := template.New("version")
+	var err error
+	if tmpl, err = tmpl.Parse(versionTmpl); err != nil {
+		log.Fatalln(err)
+	}
+
+	f, err := os.Create("util/version.go")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func(r *os.File) {
+		err = r.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(f)
+
+	tmpl.Execute(f, data)
+}
