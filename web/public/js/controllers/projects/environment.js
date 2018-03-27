a605c54583b27c0d90a6fc270f63527ee919a7e7
Artiom Stoianov <zarianec91@gmail.com>
2018-03-27 08:38:48 +0300

    Fixed SweetAlert confirmation popup

--- web/public/js/controllers/projects/environment.js
+++ web/public/js/controllers/projects/environment.js
@@ -1,78 +1,96 @@
 define(function () {
-	app.registerController('ProjectEnvironmentCtrl', ['$scope', '$http', '$uibModal', 'Project', '$rootScope', function ($scope, $http, $modal, Project, $rootScope) {
-		$scope.reload = function () {
-			$http.get(Project.getURL() + '/environment?sort=name&order=asc').then(function (environment) {
-				$scope.environment = environment.data;
-			});
-		}
+    app.registerController('ProjectEnvironmentCtrl', ['$scope', '$http', '$uibModal', 'Project', '$rootScope', function ($scope, $http, $modal, Project, $rootScope) {
+        $scope.reload = function () {
+            $http.get(Project.getURL() + '/environment?sort=name&order=asc').then(function (environment) {
+                $scope.environment = environment.data;
+            });
+        }
 
-		$scope.remove = function (environment) {
-			$http.delete(Project.getURL() + '/environment/' + environment.id).then(function () {
-				$scope.reload();
-			}).catch(function (response) {
-			  var d = response.data;
-				if (!(d && d.inUse)) {
-					swal('error', 'could not delete environment..', 'error');
-					return;
-				}
+        $scope.remove = function (environment) {
+            $http.delete(Project.getURL() + '/environment/' + environment.id)
+                .then(function () {
+                    $scope.reload();
+                })
+                .catch(function (response) {
+                    var d = response.data;
+                    if (!(d && d.inUse)) {
+                        swal('error', 'could not delete environment..', 'error');
+                        return;
+                    }
 
-				swal({
-					title: 'Environment in use',
-					text: d.error,
-					type: 'error',
-					showCancelButton: true,
-					confirmButtonColor: "#DD6B55",
-					confirmButtonText: 'Mark as removed'
-				}, function () {
-					$http.delete(Project.getURL() + '/environment/' + environment.id + '?setRemoved=1').then(function () {
-						$scope.reload();
-					}).catch(function () {
-						swal('error', 'could not delete environment..', 'error');
-					});
-				});
-			});
-		}
+                    swal({
+                        title: 'Environment in use',
+                        text: d.error,
+                        icon: 'error',
+                        buttons: {
+                            cancel: true,
+                            confirm: {
+                                text: 'Mark as removed',
+                                closeModel: false,
+                                className: 'bg-danger',
+                            }
+                        }
+                    }).then(function (value) {
+                        if (!value) {
+                            return
+                        }
 
-		$scope.add = function () {
-			var scope = $rootScope.$new();
-			scope.env = {
-				json: '{}'
-			};
+                        $http.delete(Project.getURL() + '/environment/' + environment.id + '?setRemoved=1')
+                            .then(function () {
+                                swal.stopLoading();
+                                swal.close();
 
-			$modal.open({
-				templateUrl: '/tpl/projects/environment/add.html',
-				scope: scope
-			}).result.then(function (env) {
-				$http.post(Project.getURL() + '/environment', env.environment)
-				.then(function () {
-					$scope.reload();
-				}).catch(function (response) {
-					swal('Error', 'Environment not added: ' + response.status, 'error');
-				});
-			}, function () {});
-		}
+                                $scope.reload();
+                            })
+                            .catch(function () {
+                                swal('Error', 'Could not delete environment..', 'error');
+                            });
+                    })
+                });
+        }
 
-		$scope.editEnvironment = function (env) {
-			var scope = $rootScope.$new();
-			scope.env = env;
+        $scope.add = function () {
+            var scope = $rootScope.$new();
+            scope.env = {
+                json: '{}'
+            };
 
-			$modal.open({
-				templateUrl: '/tpl/projects/environment/add.html',
-				scope: scope
-			}).result.then(function (opts) {
-				if (opts.remove) {
-					return $scope.remove(env);
-				}
+            $modal.open({
+                templateUrl: '/tpl/projects/environment/add.html',
+                scope: scope
+            }).result.then(function (env) {
+                $http.post(Project.getURL() + '/environment', env.environment)
+                    .then(function () {
+                        $scope.reload();
+                    }).catch(function (response) {
+                    swal('Error', 'Environment not added: ' + response.status, 'error');
+                });
+            }, function () {
+            });
+        }
 
-				$http.put(Project.getURL() + '/environment/' + env.id, opts.environment)
-				.then(function () {
-					$scope.reload();
-				}).catch(function (response) {
-					swal('Error', 'Environment not updated: ' + response.status, 'error');
-				});
-			}, function () {});
-		}
+        $scope.editEnvironment = function (env) {
+            var scope = $rootScope.$new();
+            scope.env = env;
 
-		$scope.reload();
-	}]);
+            $modal.open({
+                templateUrl: '/tpl/projects/environment/add.html',
+                scope: scope
+            }).result.then(function (opts) {
+                if (opts.remove) {
+                    return $scope.remove(env);
+                }
+
+                $http.put(Project.getURL() + '/environment/' + env.id, opts.environment)
+                    .then(function () {
+                        $scope.reload();
+                    }).catch(function (response) {
+                    swal('Error', 'Environment not updated: ' + response.status, 'error');
+                });
+            }, function () {
+            });
+        }
+
+        $scope.reload();
+    }]);
 });
--- web/public/js/controllers/projects/keys.js
+++ web/public/js/controllers/projects/keys.js
@@ -1,74 +1,92 @@
 define(function () {
-	app.registerController('ProjectKeysCtrl', ['$scope', '$http', '$uibModal', 'Project', '$rootScope', function ($scope, $http, $modal, Project, $rootScope) {
-		$scope.reload = function () {
-			$http.get(Project.getURL() + '/keys?sort=name&order=asc').then(function (keys) {
-				$scope.keys = keys.data;
-			});
-		}
+    app.registerController('ProjectKeysCtrl', ['$scope', '$http', '$uibModal', 'Project', '$rootScope', function ($scope, $http, $modal, Project, $rootScope) {
+        $scope.reload = function () {
+            $http.get(Project.getURL() + '/keys?sort=name&order=asc').then(function (keys) {
+                $scope.keys = keys.data;
+            });
+        }
 
-		$scope.remove = function (key) {
-			$http.delete(Project.getURL() + '/keys/' + key.id).then(function () {
-				$scope.reload();
-			}).catch(function (response) {
-			  var d = response.data;
+        $scope.remove = function (key) {
+            $http.delete(Project.getURL() + '/keys/' + key.id)
+                .then(function () {
+                    $scope.reload();
+                })
+                .catch(function (response) {
+                    var d = response.data;
 
-				if (!(d && d.inUse)) {
-					swal('error', 'could not delete key..', 'error');
-					return;
-				}
+                    if (!(d && d.inUse)) {
+                        swal('error', 'could not delete key..', 'error');
+                        return;
+                    }
 
-				swal({
-					title: 'Key in use',
-					text: d.error,
-					type: 'error',
-					showCancelButton: true,
-					confirmButtonColor: "#DD6B55",
-					confirmButtonText: 'Mark as removed'
-				}, function () {
-					$http.delete(Project.getURL() + '/keys/' + key.id + '?setRemoved=1').then(function () {
-						$scope.reload();
-					}).catch(function () {
-						swal('error', 'could not remove key..', 'error');
-					});
-				});
-			});
-		}
+                    swal({
+                        title: 'Key in use',
+                        text: d.error,
+                        icon: 'error',
+                        buttons: {
+                            cancel: true,
+                            confirm: {
+                                text: 'Mark as removed',
+                                closeModel: false,
+                                className: 'bg-danger',
+                            }
+                        }
+                    }).then(function (value) {
+                        if (!value) {
+                            return
+                        }
 
-		$scope.add = function () {
-			$modal.open({
-				templateUrl: '/tpl/projects/keys/add.html'
-			}).result.then(function (opts) {
-				$http.post(Project.getURL() + '/keys', opts.key).then(function () {
-					$scope.reload();
-				}).catch(function (response) {
-					swal('error', 'could not add key:' + response.status, 'error');
-				});
-			}, function () {});
-		}
+                        $http.delete(Project.getURL() + '/keys/' + key.id + '?setRemoved=1')
+                            .then(function () {
+                                swal.stopLoading();
+                                swal.close();
 
-		$scope.update = function (key) {
-			var scope = $rootScope.$new();
-			scope.key = key;
+                                $scope.reload();
+                            })
+                            .catch(function () {
+                                swal('Error', 'Could not remove key..', 'error');
+                            });
+                    })
+                });
+        }
 
-			$modal.open({
-				templateUrl: '/tpl/projects/keys/add.html',
-				scope: scope
-			}).result.then(function (opts) {
-				if (opts.delete) {
-					$scope.remove(key);
+        $scope.add = function () {
+            $modal.open({
+                templateUrl: '/tpl/projects/keys/add.html'
+            }).result.then(function (opts) {
+                $http.post(Project.getURL() + '/keys', opts.key).then(function () {
+                    $scope.reload();
+                }).catch(function (response) {
+                    swal('error', 'could not add key:' + response.status, 'error');
+                });
+            }, function () {
+            });
+        }
 
-					return;
-				}
+        $scope.update = function (key) {
+            var scope = $rootScope.$new();
+            scope.key = key;
 
-				$http.put(Project.getURL() + '/keys/' + key.id, opts.key)
-				.then(function () {
-					$scope.reload();
-				}).catch(function (response) {
-					swal('Error', 'could not update key:' + response.status, 'error');
-				});
-			}, function () {});
-		}
+            $modal.open({
+                templateUrl: '/tpl/projects/keys/add.html',
+                scope: scope
+            }).result.then(function (opts) {
+                if (opts.delete) {
+                    $scope.remove(key);
 
-		$scope.reload();
-	}]);
+                    return;
+                }
+
+                $http.put(Project.getURL() + '/keys/' + key.id, opts.key)
+                    .then(function () {
+                        $scope.reload();
+                    }).catch(function (response) {
+                    swal('Error', 'could not update key:' + response.status, 'error');
+                });
+            }, function () {
+            });
+        }
+
+        $scope.reload();
+    }]);
 });
