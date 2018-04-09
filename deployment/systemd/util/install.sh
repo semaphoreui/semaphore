de7f201fb661a1d13351f323b3f0d9b90a39dd86
tom whiston <tom.whiston@gmail.com>
2018-04-09 13:40:17 +0000

    daemon reload

--- deployment/systemd/util/install.sh
+++ deployment/systemd/util/install.sh
@@ -6,4 +6,5 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p /etc/semaphore
 cp ${HERE}/../semaphore.service /etc/systemd/system
 cp ${HERE}/../env /etc/semaphore/env
+systemctl daemon-reload
 systemctl start semaphore.service
