35fbd3ba2a47ba2fb8f82b9433d2a8b3e4f20f76
Denis <denguk@gmail.com>
2018-05-04 14:20:03 +0500

    Fix SweetAlert factory

--- web/public/js/factories/sweetalert.js
+++ web/public/js/factories/sweetalert.js
@@ -1,18 +1,11 @@
 app.factory('SweetAlert', [ '$rootScope', function ( $rootScope ) {
-	var swal = window.swal;
-
 	return {
-		swal: function (arg1, arg2, arg3) {
-			$rootScope.$evalAsync(function () {
-				if (typeof(arg2) === 'function') {
-					SweetAlert.swal(arg1, function (isConfirm) {
-						$rootScope.$evalAsync(function () {
-							arg2(isConfirm);
-						});
-					}, arg3);
-				} else {
-					SweetAlert.swal(arg1, arg2, arg3);
-				}
+		swal: function () {
+			var args = arguments;
+			return new Promise(function (resolve, reject) {
+				$rootScope.$evalAsync(function () {
+					window.swal.apply(null, args).then(resolve);
+				});
 			});
 		}
 	};
