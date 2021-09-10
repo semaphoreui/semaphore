2c9bc5ba0fdcf68268396130151f995569996b73
Denis Gukov <denguk@gmail.com>
2021-09-10 23:48:23 +0500

    fix(be): add \n to end of private key

--- api/tasks/runner.go
+++ api/tasks/runner.go
@@ -380,7 +380,7 @@ func (t *task) installKey(key db.AccessKey) error {
 		return fmt.Errorf("ssh key with passphrase not supported")
 	}
 
-	return ioutil.WriteFile(path, []byte(key.SshKey.PrivateKey), 0600)
+	return ioutil.WriteFile(path, []byte(key.SshKey.PrivateKey + "\n"), 0600)
 }
 
 func (t *task) updateRepository() error {
