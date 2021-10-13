ad9846e33850952f123fdafd0b903e8bc896ccfd
Denis Gukov <denguk@gmail.com>
2021-10-14 00:40:46 +0500

    feat(be): no default value for template type, null mean task

--- db/sql/migrations/v2.8.0.sql
+++ db/sql/migrations/v2.8.0.sql
@@ -1,4 +1,4 @@
-alter table project__template add `type` varchar(10) not null default 'task';
+alter table project__template add `type` varchar(10);
 alter table `task` add `message` varchar(250);
 alter table project__template add start_version varchar(20);
 alter table project__template add build_template_id int references project__template(id);
