cab1bd313dddb30780f3311bd70e260af325eff9
Denis Gukov <fiftin@outlook.com>
2022-01-19 04:03:14 +0500

    feat(fe): more suitable text for 401 error

--- web2/src/lib/error.js
+++ web2/src/lib/error.js
@@ -1,7 +1,21 @@
 // eslint-disable-next-line import/prefer-default-export
 export function getErrorMessage(err) {
-  if (err.response && err.response.data) {
-    return err.response.data.error || err.message;
+  if (err.response) {
+    if (err.response.data && err.response.data.error) {
+      return err.response.data.error;
+    }
+
+    if (err.message && !err.message.startsWith('Request failed with status code ')) {
+      return err.message;
+    }
+
+    switch (err.response.status) {
+      case 401:
+        return `${err.response.status} ${err.response.statusText}`;
+      default:
+        return err.message;
+    }
   }
+
   return err.message;
 }
