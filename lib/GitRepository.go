d2790162eb98dd67025e0ea0f62c5cbb9ba73364
Denis Gukov <fiftin@outlook.com>
2022-02-07 11:18:07 +0500

    fix(be): bug with installation and deletion git pk

--- lib/GitRepository.go
+++ lib/GitRepository.go
@@ -65,6 +65,13 @@ func (r GitRepository) run(targetDir GitRepositoryDirType, args ...string) error
 }
 
 func (r GitRepository) output(targetDir GitRepositoryDirType, args ...string) (out string, err error) {
+	err = r.Repository.SSHKey.Install(db.AccessKeyRoleGit)
+	if err != nil {
+		return
+	}
+
+	defer r.Repository.SSHKey.Destroy()
+
 	bytes, err := r.makeCmd(targetDir, args...).Output()
 	if err != nil {
 		return
