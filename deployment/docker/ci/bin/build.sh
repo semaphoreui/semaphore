f2e011a3b4dd9479a302e6b16c9e0442e46df75c
Jordan Hayes <hayesjordan@gmail.com>
2022-03-09 11:03:21 -0800

    Separate image and RPM builds

--- deployment/docker/ci/bin/build.sh
+++ deployment/docker/ci/bin/build.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -l
 
 # Set env
+SUSER=$USER
 SCRIPT_HOME=$(realpath $(dirname $0))
 SEMAPHORE_HOME=$(realpath $SCRIPT_HOME/../../../../)
 echo "Building in $SEMAPHORE_HOME"
 
-# Build
-sudo bash -lc "
+if [[ ! -f $SCRIPT_HOME/../semaphore.simg ]]; then
+    # Build Image
+    sudo bash -lc "
     # Set env
     module purge
     module load singularity || echo 'Not using modules'
     export SINGULARITY_BIND=${SEMAPHORE_HOME}:/mnt
     
     # Build image
-    cd $SCRIPT_HOME/../ && singularity build semaphore.simg Singularity || exit
+    cd $SCRIPT_HOME/../ && singularity build semaphore.simg Singularity
 
+    # Update perms
+    chmod -R ${SUSER}: $SEMAPHORE_HOME"
+else
     # Build RPM
+    module load singularity || echo 'Not using modules'
     cd ${SEMAPHORE_HOME} && singularity exec semaphore.simg task release
-"
+fi
 
