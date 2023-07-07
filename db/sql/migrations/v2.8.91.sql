61e43b0da2145e8e6f1fe6ca8c3f9cedeb37ea4e
Denis Gukov <denguk@gmail.com>
2023-07-08 00:06:43 +0200

    fix(be): specify varchat max length

--- db/sql/migrations/v2.8.91.sql
+++ db/sql/migrations/v2.8.91.sql
@@ -1,4 +1,4 @@
-ALTER TABLE project__user ADD `role` varchar NOT NULL DEFAULT 'task_runner';
+ALTER TABLE project__user ADD `role` varchar(50) NOT NULL DEFAULT 'task_runner';
 
 UPDATE project__user SET `role` = 'owner' WHERE `admin`;
 
