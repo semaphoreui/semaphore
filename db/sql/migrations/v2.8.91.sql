07b908f1ef77be23d0e95d7bb60d5397dab0f560
Denis Gukov <denguk@gmail.com>
2023-07-07 23:37:41 +0200

    fix(be): update sql query

--- db/sql/migrations/v2.8.91.sql
+++ db/sql/migrations/v2.8.91.sql
@@ -1,5 +1,5 @@
 ALTER TABLE project__user ADD `role` varchar NOT NULL DEFAULT 'task_runner';
 
-UPDATE project__user SET `role` = 'owner' WHEN `admin`;
+UPDATE project__user SET `role` = 'owner' WHERE `admin`;
 
 ALTER TABLE project__user DROP COLUMN `admin`;
