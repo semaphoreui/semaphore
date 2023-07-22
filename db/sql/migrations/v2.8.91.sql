0b81623b09a04cd9ede632614abfa11c9d2547dd
Denis Gukov <denguk@gmail.com>
2023-07-22 22:47:12 +0200

    fix(migrations): manager is default role

--- db/sql/migrations/v2.8.91.sql
+++ db/sql/migrations/v2.8.91.sql
@@ -1,4 +1,4 @@
-ALTER TABLE project__user ADD `role` varchar(50) NOT NULL DEFAULT 'task_runner';
+ALTER TABLE project__user ADD `role` varchar(50) NOT NULL DEFAULT 'manager';
 
 UPDATE project__user SET `role` = 'owner' WHERE `admin`;
 
