b1685b78a8c6cf573bca9f8f395fdc907073fd15
fiftin-quiz <157730881+fiftin-quiz@users.noreply.github.com>
2024-01-30 15:10:34 +0500

    feat(tf): implement app interface

--- db_lib/TerraformApp.go
+++ db_lib/TerraformApp.go
@@ -1,9 +1,13 @@
 package db_lib
 
 import (
+	"fmt"
 	"github.com/ansible-semaphore/semaphore/db"
 	"github.com/ansible-semaphore/semaphore/lib"
+	"github.com/ansible-semaphore/semaphore/util"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 type TerraformApp struct {
@@ -11,6 +15,47 @@ type TerraformApp struct {
 	Playbook   *AnsiblePlaybook
 	Template   db.Template
 	Repository db.Repository
+}
+
+func (t *TerraformApp) makeCmd(command string, args []string, environmentVars *[]string) *exec.Cmd {
+	cmd := exec.Command(command, args...) //nolint: gas
+	cmd.Dir = t.GetFullPath()
+
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, fmt.Sprintf("HOME=%s", util.Config.TmpPath))
+	cmd.Env = append(cmd.Env, fmt.Sprintf("PWD=%s", cmd.Dir))
+
+	if environmentVars != nil {
+		cmd.Env = append(cmd.Env, *environmentVars...)
+	}
+
+	sensitiveEnvs := []string{
+		"SEMAPHORE_ACCESS_KEY_ENCRYPTION",
+		"SEMAPHORE_ADMIN_PASSWORD",
+		"SEMAPHORE_DB_USER",
+		"SEMAPHORE_DB_NAME",
+		"SEMAPHORE_DB_HOST",
+		"SEMAPHORE_DB_PASS",
+		"SEMAPHORE_LDAP_PASSWORD",
+	}
+
+	// Remove sensitive env variables from cmd process
+	for _, env := range sensitiveEnvs {
+		cmd.Env = append(cmd.Env, env+"=")
+	}
+
+	return cmd
+}
+
+func (t *TerraformApp) runCmd(command string, args []string) error {
+	cmd := t.makeCmd(command, args, nil)
+	t.Logger.LogCmd(cmd)
+	return cmd.Run()
+}
+
+func (t *TerraformApp) GetFullPath() (path string) {
+	path = t.Repository.GetFullPath(t.Template.ID)
+	return
 }
 
 func (t *TerraformApp) SetLogger(logger lib.Logger) {
@@ -22,5 +67,13 @@ func (t *TerraformApp) InstallRequirements() error {
 }
 
 func (t *TerraformApp) Run(args []string, environmentVars *[]string, cb func(*os.Process)) error {
-	return nil
+	cmd := t.makeCmd("terraform", args, environmentVars)
+	t.Logger.LogCmd(cmd)
+	cmd.Stdin = strings.NewReader("")
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+	cb(cmd.Process)
+	return cmd.Wait()
 }
