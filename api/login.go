a33fce2fbc1f4c4e0275ce2d2f1cbcbccc8d5d35
fiftin-quiz <157730881+fiftin-quiz@users.noreply.github.com>
2024-02-01 11:47:15 +0500

    feat(login): add email suffix if oauth doesn't return email but returns username

--- api/login.go
+++ api/login.go
@@ -388,7 +388,24 @@ func claimOidcToken(idToken *oidc.IDToken, provider util.OidcProvider) (res oidc
 
 	res.email, ok = claims[provider.EmailClaim].(string)
 	if !ok {
-		err = fmt.Errorf("claim '%s' missing from id_token or not a string", provider.EmailClaim)
+
+		var username string
+		var emailSuffix string
+
+		emailSuffix, ok = claims[provider.UsernameClaim].(string)
+
+		if provider.EmailSuffix == "" {
+			err = fmt.Errorf("claim '%s' missing from id_token or not a string", provider.EmailClaim)
+		}
+
+		username, ok = claims[provider.UsernameClaim].(string)
+
+		if !ok {
+			err = fmt.Errorf("claim '%s' and '%s' missing from id_token or not a string", provider.EmailClaim, provider.UsernameClaim)
+		}
+
+		res.email = username + "@" + emailSuffix
+
 		return
 	}
 
