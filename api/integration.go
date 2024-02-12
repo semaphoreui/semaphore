39ad2c905bc1c397756414930379791fc3c3b728
Denis Gukov <fiftin@outlook.com>
2024-02-12 14:53:49 +0500

    feat(integrations): add hmac verification method

--- api/integration.go
+++ api/integration.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -14,6 +16,26 @@ import (
 	jsonq "github.com/thedevsaddam/gojsonq/v2"
 	"golang.org/x/exp/slices"
 )
+
+// IsValidPayload checks if the github payload's hash fits with
+// the hash computed by GitHub sent as a header
+func IsValidPayload(secret, headerHash string, payload []byte) bool {
+	hash := HashPayload(secret, payload)
+	return hmac.Equal(
+		[]byte(hash),
+		[]byte(headerHash),
+	)
+}
+
+// HashPayload computes the hash of payload's body according to the webhook's secret token
+// see https://developer.github.com/webhooks/securing/#validating-payloads-from-github
+// returning the hash as a hexadecimal string
+func HashPayload(secret string, playloadBody []byte) string {
+	hm := hmac.New(sha1.New, []byte(secret))
+	hm.Write(playloadBody)
+	sum := hm.Sum(nil)
+	return fmt.Sprintf("%x", sum)
+}
 
 func ReceiveIntegration(w http.ResponseWriter, r *http.Request) {
 	log.Info(fmt.Sprintf("Receiving Integration from: %s", r.RemoteAddr))
