d602cdf55238681414fa4e051e5cfe9635623140
fiftin <denguk@gmail.com>
2024-03-21 00:40:00 +0100

    feat(integration): searchable checkox

--- web/src/views/project/IntegrationExtractor.vue
+++ web/src/views/project/IntegrationExtractor.vue
@@ -1,6 +1,6 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <div>
-    <IntegrationExtractorCrumb/>
+    <IntegrationExtractorCrumb v-if="integration != null" :integration="integration"/>
 
     <div class="px-4 py-3">
       <div v-for="alias of (aliases || [])" :key="alias.id">
@@ -18,6 +18,12 @@
       <v-btn color="primary" @click="addAlias()" :disabled="aliases == null">
         {{ aliases == null ? 'Loading aliases...' : 'Add Alias' }}
       </v-btn>
+
+      <v-checkbox
+        v-model="integration.searchable"
+        label="Available by project alias"
+        @change="updateIntegration()"
+      />
     </div>
 
     <IntegrationExtractValue/>
@@ -28,6 +34,7 @@
 import IntegrationExtractorsBase from '@/components/IntegrationExtractorsBase';
 import IntegrationsBase from '@/views/project/IntegrationsBase';
 import copyToClipboard from '@/lib/copyToClipboard';
+import axios from 'axios';
 import IntegrationExtractValue from './IntegrationExtractValue.vue';
 import IntegrationMatcher from './IntegrationMatcher.vue';
 import IntegrationExtractorCrumb from './IntegrationExtractorCrumb.vue';
@@ -36,10 +43,32 @@ export default {
   mixins: [IntegrationExtractorsBase, IntegrationsBase],
   components: { IntegrationMatcher, IntegrationExtractValue, IntegrationExtractorCrumb },
 
+  data() {
+    return {
+      integration: null,
+    };
+  },
+
+  async created() {
+    this.integration = (await axios({
+      method: 'get',
+      url: `/api/project/${this.projectId}/integrations/${this.integrationId}`,
+      responseType: 'json',
+    })).data;
+  },
+
   methods: {
     copyToClipboard,
     allowActions() {
       return true;
+    },
+    async updateIntegration() {
+      await axios({
+        method: 'put',
+        url: `/api/project/${this.projectId}/integrations/${this.integrationId}`,
+        responseType: 'json',
+        data: this.integration,
+      });
     },
   },
 };
