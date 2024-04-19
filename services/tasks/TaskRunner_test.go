f348eda751d008a28bc85653acfa33b23d431834
fiftin <denguk@gmail.com>
2024-04-19 16:07:13 +0500

    test: fix tests

--- services/tasks/TaskRunner_test.go
+++ services/tasks/TaskRunner_test.go
@@ -213,7 +213,7 @@ func TestPopulateDetails(t *testing.T) {
 		Playbook:      "test.yml",
 		ProjectID:     proj.ID,
 		RepositoryID:  repo.ID,
-		InventoryID:   inv.ID,
+		InventoryID:   &inv.ID,
 		EnvironmentID: &env.ID,
 	})
 
