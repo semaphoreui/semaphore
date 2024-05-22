a67e896c1ec446e5ab045382d318637bcad182c4
fiftin <denguk@gmail.com>
2024-05-22 16:59:09 +0200

    feat: add inventory repo

--- db/Inventory.go
+++ db/Inventory.go
@@ -28,8 +28,13 @@ type Inventory struct {
 	Type InventoryType `db:"type" json:"type"`
 
 	// HolderID is an ID of template which holds the inventory
+	// It is not used now but can be used in feature for
+	// inventories which can not be used more than one template
+	// at once.
 	HolderID *int `db:"holder_id" json:"holder_id"`
 
+	// RepositoryID is an ID of repo where inventory stored.
+	// If null than inventory will be got from template repository.
 	RepositoryID *int `db:"repository_id" json:"repository_id"`
 }
 
