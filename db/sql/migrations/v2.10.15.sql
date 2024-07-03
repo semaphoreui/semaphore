82b829ebed47b7af86fc74e33b2061dda52de452
fiftin <denguk@gmail.com>
2024-07-03 13:46:24 +0500

    feat: add column task_params to integrations table

--- db/sql/migrations/v2.10.15.sql
+++ db/sql/migrations/v2.10.15.sql
@@ -1,2 +1,3 @@
 alter table `access_key` add `environment_id` int null references project__environment(`id`) on delete cascade;
 alter table `access_key` add `user_id` int null references `user`(`id`) on delete cascade;
+alter table `project__integration` add `task_params` text null;
