64b1c30e8514c246b4816523aa92aff545382d7a
Denis Gukov <fiftin@outlook.com>
2024-10-11 13:27:28 +0000

    ci(devcontainer): init

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,13 @@
 go install github.com/go-task/task/v3/cmd/task@latest
-(cd web && npm install)
+
+(cd ./web && npm install)
+
+task build
+
+./bin/semaphore user add \
+    --admin \
+    --login user123 \
+    --name User123 \
+    --email user123@example.com \
+    --password 123456 \
+    --config ./.devcontainer/config.json
