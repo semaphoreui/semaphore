151106ac3f108b19b13c0e89f68425e63a6f1d44
Denis Gukov <fiftin@outlook.com>
2024-10-13 09:26:22 +0000

    feat(runner): support flag --stdin-registration-token

--- cli/cmd/runner_register.go
+++ cli/cmd/runner_register.go
@@ -1,17 +1,39 @@
 package cmd
 
 import (
+	"io"
+	"os"
+
 	"github.com/ansible-semaphore/semaphore/services/runners"
 	"github.com/ansible-semaphore/semaphore/util"
 	"github.com/spf13/cobra"
 )
 
+var runnerRegisterArgs struct {
+	stdinRegistrationToken bool
+}
+
 func init() {
+	runnerRegisterCmd.PersistentFlags().BoolVar(&runnerRegisterArgs.stdinRegistrationToken, "stdin-registration-token", false, "Read registration token from stdin")
 	runnerCmd.AddCommand(runnerRegisterCmd)
 }
 
 func registerRunner() {
+
 	util.ConfigInit(configPath, noConfig)
+
+	if runnerRegisterArgs.stdinRegistrationToken {
+		tokenBytes, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+
+		if len(tokenBytes) == 0 {
+			panic("Empty token")
+		}
+
+		util.Config.Runner.Token = string(tokenBytes)
+	}
 
 	taskPool := runners.JobPool{}
 	err := taskPool.Register()
