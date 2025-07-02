594866e434c90b6df7f33593ee09b49d3ee5a532
Denis Gukov <denguk@gmail.com>
2025-07-02 17:33:35 +0500

    feat(storage): add storage table

--- db/sql/migrations/v2.16.0.err.sql
+++ db/sql/migrations/v2.16.0.err.sql
@@ -1,2 +1,8 @@
+alter table `access_key` drop `storage_id`;
+alter table `access_key` drop `linked_storage_id`;
+alter table `access_key` drop `linked_storage_key`;
+
+drop table secret_storage;
+
 alter table `access_key` drop `owner`;
 alter table `access_key` drop `plain`;
--- db/sql/migrations/v2.16.0.sql
+++ db/sql/migrations/v2.16.0.sql
@@ -2,5 +2,14 @@ alter table `access_key` add `owner` varchar(20) default '' not null;
 alter table `access_key` add `plain` text;
 update access_key set `owner` = 'variable' where environment_id is not null and name like 'var.%';
 update access_key set `owner` = 'environment' where environment_id is not null and name like 'env.%';
-alter table `access_key` add `source_type` varchar(20) default '' not null;
-alter table `access_key` add `source_path` varchar(1000);
+
+create table secret_storage (
+  id integer primary key autoincrement,
+  name varchar(100) not null,
+  type varchar(20) not null,
+  created_at datetime not null
+);
+
+alter table `access_key` add `storage_id` int null references `secret_storage`(`id`);
+alter table `access_key` add `linked_storage_id` int null references `secret_storage`(`id`);
+alter table `access_key` add `linked_storage_key` varchar(1000);
