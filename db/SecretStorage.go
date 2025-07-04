7b0413a4a158a46ea56c11f51e766db9b6d28529
Denis Gukov <denguk@gmail.com>
2025-07-05 01:15:10 +0500

    feat(keys): model fields

--- db/SecretStorage.go
+++ db/SecretStorage.go
@@ -3,12 +3,12 @@ package db
 type SecretStorageType string
 
 const (
-	SecretStorageVault SecretStorageType = "vault"
+	SecretStorageTypeLocal SecretStorageType = "local"
+	SecretStorageTypeVault SecretStorageType = "vault"
 )
 
 type SecretStorage struct {
-	ID        int               `db:"id" json:"id" backup:"-"`
-	ProjectID int               `db:"project_id" json:"project_id" backup:"-"`
-	Name      string            `db:"name" json:"name"`
-	Type      SecretStorageType `db:"type" json:"type"`
+	ID   int               `db:"id" json:"id"`
+	Name string            `db:"name" json:"name"`
+	Type SecretStorageType `db:"type" json:"type"`
 }
--- db/Storage.go
+++
@@ -1,4 +0,0 @@
-package db
-
-type Storage struct {
-}
