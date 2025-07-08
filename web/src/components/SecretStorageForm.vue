f1c998317075c6053067463780a3cddc501b0d39
Denis Gukov <denguk@gmail.com>
2025-07-08 15:29:01 +0500

    feat(storage): vault settings

--- web/src/components/SecretStorageForm.vue
+++ web/src/components/SecretStorageForm.vue
@@ -1,57 +1,73 @@
 <template>
   <v-form
-    ref="form"
-    lazy-validation
-    v-model="formValid"
-    v-if="item != null"
+      ref="form"
+      lazy-validation
+      v-model="formValid"
+      v-if="item != null"
   >
     <v-alert
-      :value="formError"
-      color="error"
-      class="pb-2"
-    >{{ formError }}</v-alert>
+        :value="formError"
+        color="error"
+        class="pb-2"
+    >{{ formError }}
+    </v-alert>
 
     <v-text-field
-      v-model="item.name"
-      :label="$t('name')"
-      :rules="[v => !!v || $t('name_required')]"
-      required
-      :disabled="formSaving"
-      outlined
-      dense
+        v-model="item.name"
+        :label="$t('name')"
+        :rules="[v => !!v || $t('name_required')]"
+        required
+        :disabled="formSaving"
+        outlined
+        dense
     ></v-text-field>
 
     <v-select
-      v-model="item.type"
-      :label="$t('type')"
-      :rules="[v => !!v || $t('type_required')]"
-      :items="secretStorageTypes"
-      item-value="id"
-      item-text="name"
-      required
-      :disabled="formSaving"
-      outlined
-      dense
+        v-model="item.type"
+        :label="$t('type')"
+        :rules="[v => !!v || $t('type_required')]"
+        :items="secretStorageTypes"
+        item-value="id"
+        item-text="name"
+        required
+        :disabled="formSaving"
+        outlined
+        dense
     />
 
-    <v-text-field
-      v-model="item.vault_token"
-      :label="$t('Hashicorp Vault Token')"
-      :disabled="formSaving"
-      data-testid="secretStorage-vaultToken"
-      outlined
-      dense
-      append-icon="mdi-lock"
-    ></v-text-field>
+    <div v-if="item.type === 'vault'">
 
+      <v-text-field
+          v-model="item.params.url"
+          :label="$t('Server URL')"
+          :disabled="formSaving"
+          :rules="[v => !!v || $t('url_required')]"
+          required
+          data-testid="secretStorage-vaultURL"
+          outlined
+          dense
+      ></v-text-field>
+
+      <v-text-field
+          v-model="item.vault_token"
+          :label="$t('Token')"
+          :disabled="formSaving"
+          :rules="[v => !!v || $t('token_required')]"
+          required
+          data-testid="secretStorage-vaultToken"
+          outlined
+          dense
+          append-icon="mdi-lock"
+      ></v-text-field>
+
+    </div>
   </v-form>
 </template>
 <script>
 import ItemFormBase from '@/components/ItemFormBase';
 
 export default {
-  props: {
-  },
+  props: {},
 
   mixins: [ItemFormBase],
 
@@ -65,6 +81,19 @@ export default {
   },
 
   methods: {
+
+    getNewItem() {
+      return {
+        params: {},
+      };
+    },
+
+    afterLoadData() {
+      if (!this.item.params) {
+        this.item.params = {};
+      }
+    },
+
     getItemsUrl() {
       return `/api/project/${this.projectId}/secret_storages`;
     },
