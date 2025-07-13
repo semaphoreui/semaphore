1e06c7a83d73f327f09ecd7fdcfced93ffd842cf
Denis Gukov <denguk@gmail.com>
2025-07-13 13:24:52 +0500

    fix(secrets): sqlite migration

--- db/sql/migrations/v2.16.0.sqlite.err.sql
+++ db/sql/migrations/v2.16.0.sqlite.err.sql
@@ -1,2 +1,8 @@
+alter table `access_key` drop `storage_id`;
+alter table `access_key` drop `source_storage_id`;
+alter table `access_key` drop `source_storage_key`;
+
+drop table project__secret_storage;
+
 alter table `access_key` drop `owner`;
 alter table `access_key` drop `plain`;
--- db/sql/migrations/v2.16.0.sqlite.sql
+++ db/sql/migrations/v2.16.0.sqlite.sql
@@ -2,3 +2,19 @@ alter table `access_key` add `owner` varchar(20) default '' not null;
 alter table `access_key` add `plain` text;
 update access_key set `owner` = 'variable' where environment_id is not null and name like 'var.%';
 update access_key set `owner` = 'environment' where environment_id is not null and name like 'env.%';
+
+create table project__secret_storage (
+    id integer primary key autoincrement,
+
+    project_id    int             not null,
+    name          varchar(100)    not null,
+    type          varchar(20)     not null,
+    params        text,
+    readonly      boolean         not null default false,
+
+    foreign key (`project_id`) references project(`id`) on delete cascade
+);
+
+alter table `access_key` add `storage_id` int null references `project__secret_storage`(`id`);
+alter table `access_key` add `source_storage_id` int null references `project__secret_storage`(`id`);
+alter table `access_key` add `source_storage_key` varchar(1000);
