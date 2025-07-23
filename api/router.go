7320db378c801cfecf61f07e9e188404727fa834
Denis Gukov <denguk@gmail.com>
2025-07-23 11:49:08 +0500

    fix(tasks): link to raw log"

--- api/router.go
+++ api/router.go
@@ -555,7 +555,6 @@ func serveFile(w http.ResponseWriter, r *http.Request, name string) {
 }
 
 func getSystemInfo(w http.ResponseWriter, r *http.Request) {
-	host := util.GetPublicHost()
 
 	var authMethods LoginAuthMethods
 
@@ -568,7 +567,7 @@ func getSystemInfo(w http.ResponseWriter, r *http.Request) {
 	body := map[string]any{
 		"version":           util.Version(),
 		"ansible":           util.AnsibleVersion(),
-		"web_host":          host,
+		"web_host":          util.Config.WebHost,
 		"use_remote_runner": util.Config.UseRemoteRunner,
 
 		"auth_methods": authMethods,
