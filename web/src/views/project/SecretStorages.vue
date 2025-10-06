ce9bf49198f6e9c927f97cce5b2318d21422c00c
Chris Dixon <chriswdixon@gmail.com>
2025-10-06 14:31:01 -0500

    Fix missing 'New Storage' button in Key Stores > Storages
    1. Replace hardcoded v-if='false' with proper condition
       - Change v-if='false' to v-if='can(USER_PERMISSIONS.manageProjectResources) && premiumFeatures.secret_storages'
       - This enables the New Storage button when user has permissions and secret storages are available
    
    2. The button was previously disabled due to hardcoded false condition
       - Secret storage creation is a premium feature
       - Button now properly checks both user permissions and premium feature availability
       - USER_PERMISSIONS is already available through ItemListPageBase mixin
    
    The 'New Storage' button will now appear in the Key Stores > Storages section when:
    - User has manageProjectResources permission
    - Secret storages premium feature is enabled

--- web/src/views/project/SecretStorages.vue
+++ web/src/views/project/SecretStorages.vue
@@ -42,7 +42,7 @@
 
       <v-menu
         offset-y
-        v-if="false"
+        v-if="can(USER_PERMISSIONS.manageProjectResources) && premiumFeatures.secret_storages"
       >
         <template v-slot:activator="{ on, attrs }">
 
