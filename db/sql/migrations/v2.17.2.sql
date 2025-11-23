04404d065ec76a807277a7908795eb70dfa29466
Denis Gukov <denguk@gmail.com>
2025-11-23 18:32:44 +0500

    fix: migrations

--- db/sql/migrations/v2.17.2.sql
+++ db/sql/migrations/v2.17.2.sql
@@ -1,3 +1,2 @@
-alter table `project__schedule` add column if not exists `run_at` datetime null;
-alter table `project__schedule` add column if not exists `type` varchar(20) not null default 'cron';
-update project__schedule set `type`='run_at' where `run_at` is not null and (`type`='' or `type` is null);
+alter table `project__schedule` add column `run_at` datetime null;
+alter table `project__schedule` add column `type` varchar(20) not null default 'cron';
