5d59b269c3ea68e5b41629bd6b8602dafff4d48f
Denis Gukov <denguk@gmail.com>
2025-12-15 19:50:48 +0100

    test(mcp): update playwright config

--- test/mcp/e2e/playwright.config.ts
+++ test/mcp/e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://localhost:8080',
 
