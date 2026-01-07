15615772051ca3602c52b7cc24a24fb11f3e9c11
Denis Gukov <denguk@gmail.com>
2026-01-07 21:45:56 +0500

    fix(db): sqlite migration

--- db/sql/migrations/v2.16.50.err.sql
+++ db/sql/migrations/v2.16.50.err.sql
@@ -1,1 +1,4 @@
-alter table `project__terraform_inventory_state` change `state` `state` text not null
+{{if .Sqlite}}
+{{else}}
+alter table `project__terraform_inventory_state` change `state` `state` text not null;
+{{end}}
