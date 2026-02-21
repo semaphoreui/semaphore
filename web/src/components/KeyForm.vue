fef33f25948ba5bdce465101328460449af35370
Denis Gukov <denguk@gmail.com>
2026-02-21 15:34:42 +0500

    feat(secrets): ability to choose storage type

--- web/src/components/KeyForm.vue
+++ web/src/components/KeyForm.vue
@@ -1,91 +1,96 @@
 <template>
   <v-form
-      ref="form"
-      lazy-validation
-      v-model="formValid"
-      v-if="item != null && secretStorages != null"
+    ref="form"
+    lazy-validation
+    v-model="formValid"
+    v-if="item != null && secretStorages != null"
   >
-    <v-alert
-        :value="formError"
-        color="error"
-        class="pb-2"
-    >{{ formError }}
-    </v-alert>
+    <v-alert :value="formError" color="error" class="pb-2">{{ formError }}</v-alert>
 
     <v-text-field
-        v-model="item.name"
-        :label="$t('keyName')"
-        :rules="[v => !!v || $t('name_required')]"
-        required
-        :disabled="formSaving"
-        outlined
-        dense
-    />
-
-    <v-autocomplete
-      v-if="supportStorages"
-      v-model="item.source_storage_id"
-      :label="$t('Storage (optional)')"
-      :items="secretStorages"
-      item-value="id"
-      item-text="name"
-      :disabled="formSaving || !canEditSecrets"
-      outlined
-      dense
-      clearable
-    />
-
-    <v-text-field
-      v-if="supportStorages && item.source_storage_id != null"
-      v-model="item.source_storage_key"
-      :label="$t('Source Key')"
-      :disabled="formSaving || !canEditSecrets"
+      v-model="item.name"
+      :label="$t('keyName')"
+      :rules="[(v) => !!v || $t('name_required')]"
+      required
+      :disabled="formSaving"
       outlined
       dense
     />
 
-    <v-divider class="mb-6" />
+    <v-card
+      class="mb-6"
+      :color="$vuetify.theme.dark ? '#212121' : 'white'"
+      style="background: #8585850f"
+    >
+      <v-tabs fixed-tabs v-model="sourceStorageTypeIndex">
+        <v-tab style="padding: 0">Local</v-tab>
+        <v-tab style="padding: 0">Storage</v-tab>
+        <v-tab style="padding: 0">Env</v-tab>
+        <v-tab style="padding: 0">File</v-tab>
+      </v-tabs>
+
+      <div class="ml-4 mr-4 mt-6" v-if="sourceStorageType">
+        <v-autocomplete
+          v-if="supportStorages && sourceStorageType === 'vault'"
+          v-model="item.source_storage_id"
+          :label="$t('Storage (optional)')"
+          :items="secretStorages"
+          item-value="id"
+          item-text="name"
+          :disabled="formSaving || !canEditSecrets"
+          outlined
+          dense
+          clearable
+        />
+
+        <v-text-field
+          v-if="supportStorages && sourceStorageType === 'vault' && item.source_storage_id != null"
+          v-model="item.source_storage_key"
+          :label="$t('Source Key')"
+          :disabled="formSaving || !canEditSecrets"
+          outlined
+          dense
+        />
+      </div>
+    </v-card>
 
     <v-select
-        v-model="item.type"
-        :label="$t('type')"
-        :rules="[v => (!!v || !canEditSecrets) || $t('type_required')]"
-        :items="inventoryTypes"
-        item-value="id"
-        item-text="name"
-        :required="canEditSecrets"
-        :disabled="formSaving || !canEditSecrets"
-        outlined
-        dense
+      v-model="item.type"
+      :label="$t('type')"
+      :rules="[(v) => !!v || !canEditSecrets || $t('type_required')]"
+      :items="inventoryTypes"
+      item-value="id"
+      item-text="name"
+      :required="canEditSecrets"
+      :disabled="formSaving || !canEditSecrets"
+      outlined
+      dense
     />
 
-    <v-alert
-      v-if="isReadOnly"
-      type="info"
-    >Read-only secret storage chosen.</v-alert>
+    <v-alert v-if="isReadOnly" type="info">Read-only secret storage chosen.</v-alert>
 
     <v-text-field
-        v-model="item.login_password.login"
-        :label="$t('usernameOptional')"
-        v-if="!isReadOnly && item.type === 'login_password'"
-        :disabled="formSaving || !canEditSecrets"
-        outlined
-        dense
+      v-model="item.login_password.login"
+      :label="$t('usernameOptional')"
+      v-if="!isReadOnly && item.type === 'login_password'"
+      :disabled="formSaving || !canEditSecrets"
+      outlined
+      dense
     />
 
     <v-text-field
-        v-model="item.login_password.password"
-        :append-icon="showLoginPassword ? 'mdi-eye' : 'mdi-eye-off'"
-        :label="$t('password')"
-        :rules="[v => (!!v || !canEditSecrets) || $t('password_required')]"
-        :type="showLoginPassword ? 'text' : 'password'"
-        v-if="!isReadOnly && item.type === 'login_password'"
-        :required="canEditSecrets"
-        :disabled="formSaving || !canEditSecrets"
-        autocomplete="new-password"
-        @click:append="showLoginPassword = !showLoginPassword"
-        outlined
-        dense
+      v-model="item.login_password.password"
+      :append-icon="showLoginPassword ? 'mdi-eye' : 'mdi-eye-off'"
+      :label="$t('password')"
+      :rules="[(v) => !!v || !canEditSecrets || $t('password_required')]"
+      :type="showLoginPassword ? 'text' : 'password'"
+      v-if="!isReadOnly && item.type === 'login_password'"
+      :required="canEditSecrets"
+      :disabled="formSaving || !canEditSecrets"
+      autocomplete="new-password"
+      @click:append="showLoginPassword = !showLoginPassword"
+      outlined
+      dense
     />
 
     <v-text-field
@@ -114,22 +119,13 @@
       v-model="item.ssh.private_key"
       :label="$t('privateKey')"
       :disabled="formSaving || !canEditSecrets"
-      :rules="[v => !canEditSecrets || !!v || $t('private_key_required')]"
+      :rules="[(v) => !canEditSecrets || !!v || $t('private_key_required')]"
       v-if="!isReadOnly && item.type === 'ssh'"
     />
 
-    <v-checkbox
-        v-model="item.override_secret"
-        :label="$t('override')"
-        v-if="!isNew"
-    />
+    <v-checkbox v-model="item.override_secret" :label="$t('override')" v-if="!isNew" />
 
-    <v-alert
-        dense
-        text
-        type="info"
-        v-if="item.type === 'none'"
-    >
+    <v-alert dense text type="info" v-if="item.type === 'none'">
       {{ $t('useThisTypeOfKeyForHttpsRepositoriesAndForPlaybook') }}
     </v-alert>
   </v-form>
@@ -148,22 +144,48 @@ export default {
     return {
       showLoginPassword: false,
       showSSHPassphrase: false,
-      inventoryTypes: [{
-        id: 'ssh',
-        name: `${this.$t('keyFormSshKey')}`,
-      }, {
-        id: 'login_password',
-        name: `${this.$t('keyFormLoginPassword')}`,
-      }, {
-        id: 'none',
-        name: `${this.$t('keyFormNone')}`,
-      }],
+      inventoryTypes: [
+        {
+          id: 'ssh',
+          name: `${this.$t('keyFormSshKey')}`,
+        },
+        {
+          id: 'login_password',
+          name: `${this.$t('keyFormLoginPassword')}`,
+        },
+        {
+          id: 'none',
+          name: `${this.$t('keyFormNone')}`,
+        },
+      ],
       secretStorages: null,
       // isReadOnly: true,
     };
   },
 
   computed: {
+    sourceStorageType() {
+      return this.item?.source_storage_type;
+    },
+
+    sourceStorageTypeIndex: {
+      get() {
+        return (
+          {
+            vault: 1,
+            env: 2,
+            file: 3,
+          }[this.item.source_storage_type] || 0
+        );
+      },
+      set(index) {
+        this.item = {
+          ...this.item,
+          source_storage_type: [undefined, 'vault', 'env', 'file'][index],
+        };
+      },
+    },
+
     canEditSecrets() {
       return this.isNew || this.item.override_secret;
     },
@@ -183,11 +205,7 @@ export default {
   },
 
   async created() {
-    [
-      this.secretStorages,
-    ] = await Promise.all([
-      this.loadProjectResources('secret_storages'),
-    ]);
+    [this.secretStorages] = await Promise.all([this.loadProjectResources('secret_storages')]);
   },
 
   methods: {
