286142788d1e9f94f8886ac53a846e30e460fdb9
傻笑zz <43721571+shaxiaozz@users.noreply.github.com>
2026-02-27 10:57:34 +0800

    Fix Lark alert sending method call

--- services/tasks/TaskRunner_logging.go
+++ services/tasks/TaskRunner_logging.go
@@ -128,7 +128,7 @@ func (t *TaskRunner) SetStatus(status task_logger.TaskStatus) {
 		t.sendMicrosoftTeamsAlert()
 		t.sendDingTalkAlert()
 		t.sendGotifyAlert()
-		tr.sendLarkAlert()
+		t.sendLarkAlert()
 	}
 
 	for _, l := range t.statusListeners {
