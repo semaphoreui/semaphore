d31c195c96508ffeac62d5e0e0804f04286580dc
Denis Gukov <denguk@gmail.com>
2026-03-09 08:46:40 +0100

    fix(ui): notice for empty list

--- web/src/components/SecretStorageSyncOptionsForm.vue
+++ web/src/components/SecretStorageSyncOptionsForm.vue
@@ -1,35 +1,38 @@
 <template>
   <div>
+    <v-alert text v-if="!paths || paths.length === 0">
+      {{ $t('No sync paths are defined.') }}
+    </v-alert>
     <div v-for="(p, i) in paths" :key="i" class="d-flex align-start mb-4">
       <div style="flex: 1; min-width: 0" class="mb-2">
         <v-text-field
-            class="mb-2"
-            v-model="p.path"
-            :label="$t('path')"
-            outlined
-            dense
-            @input="emitPaths"
-            hide-details
+          class="mb-2"
+          v-model="p.path"
+          :label="$t('path')"
+          outlined
+          dense
+          @input="emitPaths"
+          hide-details
         />
 
         <div class="d-flex" style="gap: 8px">
           <v-text-field
-              v-model="p.separator"
-              :label="$t('separator')"
-              outlined
-              dense
-              style="flex: 1"
-              @input="emitPaths"
-              hide-details
+            v-model="p.separator"
+            :label="$t('separator')"
+            outlined
+            dense
+            style="flex: 1"
+            @input="emitPaths"
+            hide-details
           />
           <v-text-field
-              v-model="p.prefix"
-              :label="$t('prefix')"
-              outlined
-              dense
-              style="flex: 1"
-              @input="emitPaths"
-              hide-details
+            v-model="p.prefix"
+            :label="$t('prefix')"
+            outlined
+            dense
+            style="flex: 1"
+            @input="emitPaths"
+            hide-details
           />
         </div>
       </div>
@@ -40,10 +43,10 @@
     </div>
 
     <v-btn
-        text
-        color="primary"
-        @click="addPath"
-        style="position: absolute; left: 12px; bottom: 12px"
+      text
+      color="primary"
+      @click="addPath"
+      style="position: absolute; left: 12px; bottom: 12px"
     >
       <v-icon left>mdi-plus</v-icon>
       Add path
@@ -67,9 +70,7 @@ export default {
   },
 
   created() {
-    this.paths = this.value && this.value.length
-      ? this.value.map((p) => ({ ...p }))
-      : [];
+    this.paths = this.value && this.value.length ? this.value.map((p) => ({ ...p })) : [];
   },
 
   watch: {
@@ -82,7 +83,10 @@ export default {
 
   methods: {
     emitPaths() {
-      this.$emit('input', this.paths.map((p) => ({ ...p })));
+      this.$emit(
+        'input',
+        this.paths.map((p) => ({ ...p })),
+      );
     },
 
     addPath() {
