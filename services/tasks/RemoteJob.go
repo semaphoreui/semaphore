0252b9c5d88b650a313142ca768c01e105c3b4c9
Denis Gukov <denguk@gmail.com>
2026-03-31 21:12:13 +0200

    fix: missed import

--- services/tasks/RemoteJob.go
+++ services/tasks/RemoteJob.go
@@ -14,6 +14,7 @@ import (
 	"github.com/semaphoreui/semaphore/pkg/task_logger"
 	"github.com/semaphoreui/semaphore/pkg/tz"
 	"github.com/semaphoreui/semaphore/util"
+	log "github.com/sirupsen/logrus"
 )
 
 // ErrAllRunnersBusy is returned when all available runners are busy. Used for logic
