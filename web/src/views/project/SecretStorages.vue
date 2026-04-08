39349b078a58d2902f160e9f2559c0baa7dc4439
Denis Gukov <denguk@gmail.com>
2026-04-08 16:40:19 +0200

    feat(azure): imlement azure storage

--- web/src/views/project/SecretStorages.vue
+++ web/src/views/project/SecretStorages.vue
@@ -1,35 +1,35 @@
 <template>
   <div v-if="items != null">
     <ObjectRefsDialog
-        object-title="storage"
-        :object-refs="itemRefs"
-        :project-id="projectId"
-        v-model="itemRefsDialog"
+      object-title="storage"
+      :object-refs="itemRefs"
+      :project-id="projectId"
+      v-model="itemRefsDialog"
     />
 
     <YesNoDialog
-        :title="$t('deleteStorage')"
-        :text="$t('askDeleteStorage')"
-        v-model="deleteItemDialog"
-        @yes="deleteItem(itemId)"
+      :title="$t('deleteStorage')"
+      :text="$t('askDeleteStorage')"
+      v-model="deleteItemDialog"
+      @yes="deleteItem(itemId)"
     />
 
     <EditDialog
-        v-model="editDialog"
-        :save-button-text="itemId === 'new' ? $t('create') : $t('save')"
-        :title="`${itemId === 'new' ? $t('nnew') : $t('edit')} ${itemType} Storage`"
-        :max-width="450"
-        @save="loadItems()"
+      v-model="editDialog"
+      :save-button-text="itemId === 'new' ? $t('create') : $t('save')"
+      :title="`${itemId === 'new' ? $t('nnew') : $t('edit')} ${itemType} Storage`"
+      :max-width="450"
+      @save="loadItems()"
     >
       <template v-slot:form="{ onSave, onError, needSave, needReset }">
         <SecretStorageForm
-            :project-id="projectId"
-            :item-id="itemId"
-            :item-type="itemType"
-            @save="onSave"
-            @error="onError"
-            :need-save="needSave"
-            :need-reset="needReset"
+          :project-id="projectId"
+          :item-id="itemId"
+          :item-type="itemType"
+          @save="onSave"
+          @error="onError"
+          :need-save="needSave"
+          :need-reset="needReset"
         />
       </template>
     </EditDialog>
@@ -42,11 +42,11 @@
       <v-menu offset-y>
         <template v-slot:activator="{ on, attrs }">
           <v-btn
-              class="pr-2"
-              v-bind="attrs"
-              v-on="on"
-              color="primary"
-              v-if="can(USER_PERMISSIONS.manageProjectResources)"
+            class="pr-2"
+            v-bind="attrs"
+            v-on="on"
+            color="primary"
+            v-if="can(USER_PERMISSIONS.manageProjectResources)"
           >
             New Storage
             <v-icon>mdi-chevron-down</v-icon>
@@ -54,12 +54,12 @@
         </template>
         <v-list>
           <v-list-item
-              link
-              @click="
+            link
+            @click="
               editItem('new');
               itemType = 'vault';
             "
-              :disabled="!premiumFeatures.secret_storage_management"
+            :disabled="!premiumFeatures.secret_storage_management"
           >
             <v-list-item-icon>
               <v-icon>$vuetify.icons.hashicorp_vault</v-icon>
@@ -68,31 +68,45 @@
           </v-list-item>
 
           <v-list-item
-              link
-              @click="
-              editItem('new');
-              itemType = 'dvls';
-            "
-              :disabled="!premiumFeatures.secret_storage_management"
-          >
-            <v-list-item-icon>
-              <v-icon>$vuetify.icons.dvls</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Devolutions Server</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item
-              link
-              @click="
+            link
+            @click="
               editItem('new');
               itemType = 'aws_sm';
             "
-              :disabled="!premiumFeatures.secret_storage_management"
+            :disabled="!premiumFeatures.secret_storage_management"
           >
             <v-list-item-icon>
               <v-icon>$vuetify.icons.aws_sm</v-icon>
             </v-list-item-icon>
             <v-list-item-title>AWS Secrets Manager</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item
+            link
+            @click="
+              editItem('new');
+              itemType = 'azure_kv';
+            "
+            :disabled="!premiumFeatures.secret_storage_management"
+          >
+            <v-list-item-icon>
+              <v-icon>$vuetify.icons.azure_kv</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Azure Key Vault</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item
+            link
+            @click="
+              editItem('new');
+              itemType = 'dvls';
+            "
+            :disabled="!premiumFeatures.secret_storage_management"
+          >
+            <v-list-item-icon>
+              <v-icon>$vuetify.icons.dvls</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Devolutions Server</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
@@ -104,21 +118,21 @@
       </v-tab>
 
       <v-tab
-          key="storages"
-          :to="`/project/${projectId}/secret_storages`"
-          data-testid="keystore-storages"
+        key="storages"
+        :to="`/project/${projectId}/secret_storages`"
+        data-testid="keystore-storages"
       >
         Storages
       </v-tab>
     </v-tabs>
 
-    <v-divider style="margin-top: -1px"/>
+    <v-divider style="margin-top: -1px" />
 
     <v-alert
-        v-if="!premiumFeatures.secret_storage_management"
-        text
-        color="hsl(348deg, 86%, 61%)"
-        class="PageAlert"
+      v-if="!premiumFeatures.secret_storage_management"
+      text
+      color="hsl(348deg, 86%, 61%)"
+      class="PageAlert"
     >
       <span class="mr-1" v-html="$t('secret_storage_only_pro')"></span>
 
@@ -132,15 +146,12 @@
     </v-alert>
 
     <v-data-table
-        :headers="headers"
-        :items="items"
-        hide-default-footer
-        class="mt-4"
-        :items-per-page="Number.MAX_VALUE"
-        style="
-          max-width: calc(var(--breakpoint-xl) - var(--nav-drawer-width) - 200px);
-          margin: auto
-        "
+      :headers="headers"
+      :items="items"
+      hide-default-footer
+      class="mt-4"
+      :items-per-page="Number.MAX_VALUE"
+      style="max-width: calc(var(--breakpoint-xl) - var(--nav-drawer-width) - 200px); margin: auto"
     >
       <template v-slot:item.name="{ item }">
         <v-icon class="mr-3" small>
@@ -162,8 +173,10 @@
         <v-btn-toggle dense :value-comparator="() => false" style="">
           <v-btn
             @click="syncItem(item.id)"
-            :disabled="(item.type !== 'dvls' && item.type !== 'aws_sm') ||
-              !(item.params.sync_paths && item.params.sync_paths.length > 0)"
+            :disabled="
+              (item.type !== 'dvls' && item.type !== 'aws_sm' && item.type !== 'azure_kv') ||
+              !(item.params.sync_paths && item.params.sync_paths.length > 0)
+            "
           >
             <v-icon>mdi-sync</v-icon>
           </v-btn>
@@ -238,6 +251,8 @@ export default {
           return '$vuetify.icons.dvls';
         case 'aws_sm':
           return '$vuetify.icons.aws_sm';
+        case 'azure_kv':
+          return '$vuetify.icons.azure_kv';
         default:
           return '';
       }
