e088bc6ee6672fd60d22e6f4725dc94c8eb8ebc2
Denis Gukov <denguk@gmail.com>
2026-04-25 01:35:56 +0200

    feat(api): token dialog style

--- web/src/views/Tokens.vue
+++ web/src/views/Tokens.vue
@@ -3,7 +3,7 @@
     <v-dialog v-model="newTokenDialog" max-width="480" persistent>
       <v-card>
         <v-card-title>{{ $t('New Token') }}</v-card-title>
-        <v-card-text>
+        <v-card-text class="pb-0 pt-4">
           <v-select
             v-model="expiresInDays"
             :items="expiryOptions"
@@ -22,10 +22,10 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn text @click="newTokenDialog = false" :disabled="creatingToken">
+          <v-btn text color="primary" @click="newTokenDialog = false" :disabled="creatingToken">
             {{ $t('Cancel') }}
           </v-btn>
-          <v-btn color="primary" @click="createToken()" :loading="creatingToken">
+          <v-btn text color="primary" @click="createToken()" :loading="creatingToken">
             {{ $t('Create') }}
           </v-btn>
         </v-card-actions>
@@ -33,11 +33,7 @@
     </v-dialog>
 
     <v-toolbar flat>
-      <v-btn
-        icon
-        class="mr-4"
-        @click="returnToProjects()"
-      >
+      <v-btn icon class="mr-4" @click="returnToProjects()">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
       <v-toolbar-title>{{ $t('api_tokens') }}</v-toolbar-title>
@@ -51,10 +47,7 @@
         {{ $t('API Reference') }}
       </a>
 
-      <v-btn
-        color="primary"
-        @click="newToken()"
-      >{{ $t('New Token') }}</v-btn>
+      <v-btn color="primary" @click="newToken()">{{ $t('New Token') }}</v-btn>
     </v-toolbar>
 
     <v-divider />
@@ -65,17 +58,11 @@
       class="mt-4"
       :footer-props="{ itemsPerPageOptions: [20] }"
     >
-
       <template v-slot:item.id="{ item }">
-
         <code v-if="item.token_id && item.show_token_id" class="mr-2">{{ item.token_id }}</code>
         <code v-else class="mr-2">{{ item.id }}***</code>
 
-        <v-btn
-          icon
-          v-if="item.token_id && !item.show_token_id"
-          @click="showToken(item.id)"
-        >
+        <v-btn icon v-if="item.token_id && !item.show_token_id" @click="showToken(item.id)">
           <v-icon>mdi-eye</v-icon>
         </v-btn>
 
@@ -84,11 +71,10 @@
           :text="item.token_id"
           success-message="The token has been copied to the clipboard."
         />
-
       </template>
 
       <template v-slot:item.created="{ item }">
-        {{ item.created | formatDate}}
+        {{ item.created | formatDate }}
       </template>
 
       <template v-slot:item.expires_at="{ item }">
@@ -98,13 +84,13 @@
 
       <template v-slot:item.expired="{ item }">
         <div class="pr-4">
-          <v-chip v-if="item.expired" style="font-weight: bold;" color="error">
+          <v-chip v-if="item.expired" style="font-weight: bold" color="error">
             {{ $t('Revoked') }}
           </v-chip>
-          <v-chip v-else-if="isExpiredByTime(item)" style="font-weight: bold;" color="warning">
+          <v-chip v-else-if="isExpiredByTime(item)" style="font-weight: bold" color="warning">
             {{ $t('Expired') }}
           </v-chip>
-          <v-chip v-else style="font-weight: bold;" color="success">
+          <v-chip v-else style="font-weight: bold" color="success">
             {{ $t('Active') }}
           </v-chip>
         </div>
@@ -112,11 +98,7 @@
 
       <template v-slot:item.actions="{ item }">
         <div style="white-space: nowrap">
-          <v-btn
-            icon
-            class="mr-1"
-            @click="deleteItem(item.id)"
-          >
+          <v-btn icon class="mr-1" @click="deleteItem(item.id)">
             <v-icon>mdi-delete</v-icon>
           </v-btn>
         </div>
@@ -163,7 +145,6 @@ export default {
   },
 
   methods: {
-
     isExpiredByTime(item) {
       if (!item.expires_at) return false;
       return new Date(item.expires_at).getTime() <= Date.now();
@@ -202,12 +183,14 @@ export default {
 
       this.creatingToken = true;
       try {
-        const res = (await axios({
-          method: 'post',
-          url: '/api/user/tokens',
-          responseType: 'json',
-          data: { expires_at: expiresAt },
-        })).data;
+        const res = (
+          await axios({
+            method: 'post',
+            url: '/api/user/tokens',
+            responseType: 'json',
+            data: { expires_at: expiresAt },
+          })
+        ).data;
         await this.loadItems();
 
         const i = this.items.findIndex((item) => res.id.startsWith(item.id));
@@ -224,24 +207,30 @@ export default {
     },
 
     getHeaders() {
-      return [{
-        text: this.$i18n.t('token'),
-        value: 'id',
-      }, {
-        text: this.$i18n.t('created'),
-        value: 'created',
-      }, {
-        text: this.$i18n.t('Expires'),
-        value: 'expires_at',
-      }, {
-        text: this.$i18n.t('status'),
-        value: 'expired',
-      }, {
-        text: '',
-        value: 'actions',
-        sortable: false,
-        width: 70,
-      }];
+      return [
+        {
+          text: this.$i18n.t('token'),
+          value: 'id',
+        },
+        {
+          text: this.$i18n.t('created'),
+          value: 'created',
+        },
+        {
+          text: this.$i18n.t('Expires'),
+          value: 'expires_at',
+        },
+        {
+          text: this.$i18n.t('status'),
+          value: 'expired',
+        },
+        {
+          text: '',
+          value: 'actions',
+          sortable: false,
+          width: 70,
+        },
+      ];
     },
 
     async returnToProjects() {
