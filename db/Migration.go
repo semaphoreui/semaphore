eeb5b58c378619916d4bb1e09283fb7f481e5482
Denis Gukov <denguk@gmail.com>
2026-05-01 10:41:49 +0200

    fix: merge conflict

--- db/Migration.go
+++ db/Migration.go
@@ -121,6 +121,11 @@ func GetMigrations(dialect string) []Migration {
 		{Version: "2.17.2"},
 		{Version: "2.17.15"},
 		{Version: "2.17.16"},
+		{Version: "2.17.17"},
+		{Version: "2.18.1"},
+		{Version: "2.18.2"},
+		{Version: "2.18.5"},
+		{Version: "2.18.6"},
 	}
 
 	return append(initScripts, commonScripts...)
