632618b5ae47b9c74836834b9f1f93be536578ae
Denis Gukov <denguk@gmail.com>
2026-06-07 01:58:22 +0500

    fix(runners): do not reset Active on gen reg token

--- db/sql/global_runner.go
+++ db/sql/global_runner.go
@@ -222,7 +222,7 @@ func (d *SqlDb) RegisterRunner(registrationTokenHash string, publicKey *string) 
 
 func (d *SqlDb) ResetRunnerRegistration(runnerID int, registrationTokenHash string, expiresAt time.Time) (err error) {
 	_, err = d.exec(
-		"update `runner` set `token`='', `active`=false, `public_key`=null, `registration_token`=?, `registration_token_expires_at`=? where id=?",
+		"update `runner` set `token`='', `public_key`=null, `registration_token`=?, `registration_token_expires_at`=? where id=?",
 		registrationTokenHash,
 		expiresAt,
 		runnerID)
