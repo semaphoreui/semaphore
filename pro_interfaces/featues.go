74d8dd572c056ce943080a344d87e29dd036df33
Denis Gukov <denguk@gmail.com>
2026-07-09 01:24:00 +0500

    feat(ui): placeholder for enterprise storages

--- pro_interfaces/featues.go
+++ pro_interfaces/featues.go
@@ -1,11 +1,12 @@
 package pro_interfaces
 
 type Features struct {
-	ProjectRunners          bool `json:"project_runners"`
-	TerraformBackend        bool `json:"terraform_backend"`
-	TaskSummary             bool `json:"task_summary"`
-	SecretStorages          bool `json:"secret_storages"`
-	SecretStorageManagement bool `json:"secret_storage_management"`
-	CustomRolesManagement   bool `json:"custom_roles_management"`
-	HighAvailability        bool `json:"high_availability"`
+	ProjectRunners            bool `json:"project_runners"`
+	TerraformBackend          bool `json:"terraform_backend"`
+	TaskSummary               bool `json:"task_summary"`
+	SecretStorages            bool `json:"secret_storages"`
+	SecretStorageManagement   bool `json:"secret_storage_management"`
+	SecretStorageManagementEx bool `json:"secret_storage_management_ex"`
+	CustomRolesManagement     bool `json:"custom_roles_management"`
+	HighAvailability          bool `json:"high_availability"`
 }
