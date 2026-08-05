59615cfa5767e4cb5bf7ec234a149a0f0e1f48c1
Befikadu <befika77@gmail.com>
2026-08-05 14:50:59 +0300

    feat: add proxy management capabilities and update API documentation

--- .dredd/hooks/capabilities.go
+++ .dredd/hooks/capabilities.go
@@ -31,6 +31,7 @@ var workflowApproval *db.WorkflowApproval
 
 // Runtime created simple ID values for some items we need to reference in other objects
 var repoID int
+var proxyID int
 var inventoryID int
 var environmentID int
 var templateID int
@@ -45,6 +46,7 @@ var capabilities = map[string][]string{
 	"user":                    {},
 	"project":                 {"user"},
 	"repository":              {"access_key"},
+	"proxy":                   {},
 	"inventory":               {"repository"},
 	"environment":             {"repository"},
 	"template":                {"repository", "inventory", "environment", "view"},
@@ -116,6 +118,20 @@ func resolveCapability(caps []string, resolved []string, uid string) {
 			})
 			printError(err)
 			repoID = pRepo.ID
+		case "proxy":
+			port := 22
+			user := "ansible-proxy"
+			res, err := store.CreateProxy(db.Proxy{
+				ProjectID: userProject.ID,
+				Name:      "ITP-" + uid,
+				Type:      db.ProxySSH,
+				Host:      "bastion.example.org",
+				Port:      &port,
+				User:      &user,
+				SSHKeyID:  &userKey.ID,
+			})
+			printError(err)
+			proxyID = res.ID
 		case "inventory":
 			res, err := store.CreateInventory(db.Inventory{
 				ProjectID:    userProject.ID,
@@ -297,6 +313,7 @@ func alterRequestBody(t *trans.Transaction) {
 	bodyFieldProcessor("environment_id", environmentID, &request)
 	bodyFieldProcessor("environment_ids", []int{environmentID}, &request)
 	bodyFieldProcessor("inventory_id", inventoryID, &request)
+	bodyFieldProcessor("proxy_id", proxyID, &request)
 	bodyFieldProcessor("repository_id", repoID, &request)
 	bodyFieldProcessor("template_id", templateID, &request)
 	bodyFieldProcessor("build_template_id", nil, &request)
