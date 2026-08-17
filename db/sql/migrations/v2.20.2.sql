1b1ffbc57f8aa01089d8863a9c1901d3220a7eb2
Nick <lowswoo@gmail.com>
2026-08-17 06:52:09 +0000

    fix: add unique key to workflow_node_id in migrations

--- db/sql/migrations/v2.20.2.sql
+++ db/sql/migrations/v2.20.2.sql
@@ -12,7 +12,8 @@ create table `project__workflow_delay` (
 
   foreign key (`project_id`) references `project`(`id`) on delete cascade,
   foreign key (`workflow_run_id`) references `project__workflow_run`(`id`) on delete cascade,
-  foreign key (`workflow_node_id`) references `project__workflow_node`(`id`) on delete cascade
+  foreign key (`workflow_node_id`) references `project__workflow_node`(`id`) on delete cascade,
+  unique (`workflow_run_id`, `workflow_node_id`)
 );
 
 create index `project__workflow_delay__workflow_run_id` on `project__workflow_delay`(`workflow_run_id`);
