532b357af4e957d07e160a57104debcc8a02aa89
Rustam Zaitov <r-zaitov@yandex.ru>
2026-09-08 11:00:00 +0200

    fix(dredd): isolate and cleanly terminate test server

--- .dredd/server-wrapper.sh
+++ .dredd/server-wrapper.sh
@@ -2,8 +2,9 @@
 
 export SEMAPHORE_MAX_TASKS_PER_TEMPLATE=300
 export SEMAPHORE_APPS='{"ansible": {}}'
+export SEMAPHORE_PORT=58427
 
 semaphore=./semaphore
 [[ -x "$semaphore" ]] || semaphore=./bin/semaphore
 
-"$semaphore" server --config .dredd/config.json
+exec "$semaphore" server --config .dredd/config.json
